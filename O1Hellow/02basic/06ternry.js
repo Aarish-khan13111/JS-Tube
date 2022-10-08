f37e164daa17390ea78a7bd389fd2298416d9a72
Aarish khan <aarishkhan13111@gmail.com>
2022-10-08 19:47:38 +0530

    edited index.js

--- O1Hellow/02basic/06ternry.js
+++ O1Hellow/02basic/06ternry.js
@@ -1,12 +1,10 @@
 var authenticated = true;
 
-
 // if (authenticated) {
 //     console.log("show singout button");
 // }else{
 //     console.log("show loggin option");
 
 // }
-
 
 authenticated ? console.log("singOut Button") : console.log("login");
